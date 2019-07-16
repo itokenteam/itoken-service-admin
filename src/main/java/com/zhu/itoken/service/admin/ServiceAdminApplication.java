3105a4aef3b9be1750225ada6bedc47735d8eb65
= <=>
2019-07-16 22:48:18 +0800

    itoken-service-admin

---
+++ src/main/java/com/zhu/itoken/service/admin/ServiceAdminApplication.java
@@ -0,0 +1,19 @@
+package com.zhu.itoken.service.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@MapperScan(basePackages = "com.zhu.itoken.service.admin.mapper")
+public class ServiceAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+}
---
+++ src/main/java/com/zhu/itoken/service/admin/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zhu.itoken.service.admin.mapper;
+
+import com.zhu.itoken.service.admin.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
